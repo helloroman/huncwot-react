5ea7b3f578b4b2c4e739546fe4fcdec85fe4ea75
Adam Romański <adam.romanski@netguru.pl>
2020-01-17 11:56:20 +0100

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const consts = {
+  appUrl: 'https://api.punkapi.com/v2/beers',
+};
---
+++ src/views/Root.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Root = () => {
+  return <div>lorem ipsum</div>
+};
+
+export default Root;
